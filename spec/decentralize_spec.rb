c7bcaba1b4f62d212217fe9f7175ba917cb6efa7
Shane Becker <veganstraightedge@gmail.com>
2025-10-17 03:32:05 -0700

    Rubocop: RSpec/BeNil

--- spec/decentralize_spec.rb
+++ spec/decentralize_spec.rb
@@ -2,10 +2,10 @@
 
 RSpec.describe Decentralize do
   it "has a version number" do
-    expect(Decentralize::VERSION).not_to be nil
+    expect(Decentralize::VERSION).not_to be_nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(false).to be(true)
   end
 end
