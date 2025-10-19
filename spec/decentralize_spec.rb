6b752a9ba0769a40e12dbc04a54a733a9245e435
Shane Becker <veganstraightedge@gmail.com>
2025-10-18 17:38:36 -0700

    Remove useless test

--- spec/decentralize_spec.rb
+++ spec/decentralize_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Decentralize do
   it 'has a version number' do
     expect(Decentralize::VERSION).not_to be_nil
   end
-
-  it 'does something useful' do
-    expect(false).to be(true)
-  end
 end
